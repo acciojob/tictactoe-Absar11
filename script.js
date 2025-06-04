50f0df76f9fb96980d30f2da19dd6e567db5c046
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-06-04 11:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const player1Input = document.getElementById("player-1");
-const player2Input = document.getElementById("player-2");
+const player1Input = document.getElementById("player1");
+const player2Input = document.getElementById("player2");
 const startBtn = document.getElementById("submit");
 let formContainer = document.getElementById("formContainer");
 let gameContainer = document.getElementById("gameContainer");
